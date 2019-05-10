72a790949cc3f5572b8f6a3a4f229728d3bcdd0c
IT-Thinker <thinkerhuang@efun.com>
2019-05-10 17:40:32 +0800

    fix bug

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,67 +5,74 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const phaserModulePath = path.join(__dirname, '/node_modules/phaser-ce/');
 const webpack = require('webpack');
 const CopyPlugin = require('copy-webpack-plugin');
-module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, './src/ts/index.ts'),
-    watch: true,
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/[name].js',
-        chunkFilename: '[name].js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpg|gif)$/, //匹配所有格式的图片资源
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: 'assets/images/[name].[ext]',
-                            publicPath: '../',
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.scss$/,
-                // 因为这个插件需要干涉模块转换的内容，所以需要使用它对应的 loader
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader'],
-                }),
-            },
-            {
-                // Include ts, tsx, js, and jsx files.
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                include: [path.resolve(__dirname, 'src/ts')],
-                use: ['babel-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html', // 配置输出文件名和路径
-            template: './index.html', // 配置文件模板
-        }),
-        new ExtractTextPlugin('css/[name].css'),
-    ],
-    resolve: {
-        modules: ['node_modules', path.resolve(__dirname, 'src')],
-        extensions: ['.json', '.js', '.jsx', '.ts', '.tsx'],
-        alias: {
-            '@images': path.resolve(__dirname, 'assets/images'),
-            '@assets': path.resolve(__dirname, 'assets'),
+module.exports = (env, argv) => {
+    return {
+        mode: 'development',
+        entry: path.resolve(__dirname, './src/ts/index.ts'),
+        watch: true,
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: './js/index.js',
+            chunkFilename: 'index.js',
         },
-    },
-    devtool: 'source-maps',
-    devServer: {
-        // contentBase: path.join('/dist/'),
-        stats: 'errors-only',
-        inline: true,
-        host: '127.0.0.1',
-        port: 5050,
-    },
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpg|gif)$/, //匹配所有格式的图片资源
+                    use: [
+                        {
+                            loader: 'file-loader',
+                            options: {
+                                name: 'assets/images/[name].[ext]',
+                                publicPath: '../',
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.scss$/,
+                    // 因为这个插件需要干涉模块转换的内容，所以需要使用它对应的 loader
+                    use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: ['css-loader', 'sass-loader'],
+                    }),
+                },
+                {
+                    // Include ts, tsx, js, and jsx files.
+                    test: /\.(ts|js)x?$/,
+                    exclude: /node_modules/,
+                    include: [path.resolve(__dirname, 'src/ts')],
+                    use: ['babel-loader'],
+                },
+            ],
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                GLOBALWIDTH: env.direction === 'vertical' ? 320 : 480,
+                GLOBALHEIGHT: env.direction === 'vertical' ? 480 : 320,
+            }),
+            new HtmlWebpackPlugin({
+                filename: 'index.html', // 配置输出文件名和路径
+                template: './index.ejs', // 配置文件模板
+                chunks: ['index'],
+            }),
+            new ExtractTextPlugin('css/[name].css'),
+        ],
+        resolve: {
+            modules: ['node_modules', path.resolve(__dirname, 'src')],
+            extensions: ['.json', '.js', '.jsx', '.ts', '.tsx'],
+            alias: {
+                '@images': path.resolve(__dirname, 'assets/images'),
+                '@assets': path.resolve(__dirname, 'assets'),
+            },
+        },
+        devtool: 'source-maps',
+        devServer: {
+            // contentBase: path.join('/dist/'),
+            stats: 'errors-only',
+            inline: true,
+            host: '127.0.0.1',
+            port: 5050,
+        },
+    };
 };
